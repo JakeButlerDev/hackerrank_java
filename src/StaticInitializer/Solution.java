aa7e3eba129f38bf91490ee72ca28b41f9252795
Jake Butler <JakeButler.student@careerdevs.com>
2022-10-16 22:31:54 -0400

    Completed one exercise

---
+++ src/StaticInitializer/Solution.java
@@ -0,0 +1,19 @@
+package StaticInitializer;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner keyboard = new Scanner(System.in);
+        int B = keyboard.nextInt();
+        int H = keyboard.nextInt();
+        if (B > 0 && H > 0) {
+            System.out.println(B * H);
+        } else {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+}
